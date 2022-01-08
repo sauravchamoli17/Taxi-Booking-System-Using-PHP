735522e67629c6901b437498669e03fe42ee69e2
Saurav Chamoli <42397262+sauravchamoli17@users.noreply.github.com>
2022-01-08 10:08:27 +0530

    Update maps.js

--- maps.js
+++ maps.js
@@ -3,7 +3,7 @@ function getRoute(pincode){
     let latitude,longitude;
     async function pincodeToCoordinates(){
     async function getCoordinates(address){       
-    const response= await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyC3nSH3nD_lgHPgXS6Ci5DYEpONbpK1j70`);
+    const response= await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${YOUR-API-KEY}`);
         const data = await response.json();
         latitude=data.results[0].geometry.location.lat;
         longitude=data.results[0].geometry.location.lng;
